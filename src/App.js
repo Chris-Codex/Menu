19f405a8ea63fcc856036ee695c51fbc001373d9
Christian Obinna Iheacho <christianobinna.iheacho@student.griffith.ie>
2022-02-10 23:26:47 +0000

    fetch Api data

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from "react";
+import { FaAngleDoubleRight } from "react-icons/fa";
 import "./css/App.css";
 
-const url = "https://course-api.netlify.api/api/react-tabs-project";
+const url = "https://course-api.com/react-tabs-project";
 
 function App() {
   const [loading, setLoading] = useState(true);
@@ -22,15 +23,46 @@ function App() {
   if (loading) {
     return (
       <section className="section loading">
-        <h1>loading...</h1>
+        <div className="home-container container">
+          <h1>loading...</h1>
+        </div>
       </section>
     );
   }
 
+  const { company, dates, duties, title } = jobs[value];
   return (
-    <div className="container">
-      <h2>Jobs</h2>
-    </div>
+    <section className="section">
+      <div className="home-container container">
+        <div className="row">
+          <div className="col-md-4">wqeqeq</div>
+          <div className="col-md-8">
+            <h3 className="experience">Experience</h3>
+            <div className="experience-wrapper">
+              <p className="title">{title}</p>
+              <button class="btn btn-light btn-name">
+                <h6>{company}</h6>
+              </button>
+              <p className="date"> {dates}</p>
+              {duties.map((duty, index) => {
+                return (
+                  <div className="job-desc" key={index}>
+                    <FaAngleDoubleRight className="job-icon"></FaAngleDoubleRight>
+                    <p style={{ marginLeft: "20px", marginTop: "-24px" }}>
+                      {duty}
+                    </p>
+                  </div>
+                );
+              })}
+            </div>
+
+            <button type="button" class="btn btn-light more-info">
+              More Info
+            </button>
+          </div>
+        </div>
+      </div>
+    </section>
   );
 }
 
