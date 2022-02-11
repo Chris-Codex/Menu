0220283186363ac5d1252e005f19a9193c2ce58e
Christian Obinna Iheacho <christianobinna.iheacho@student.griffith.ie>
2022-02-11 10:53:21 +0000

    Implementation of destructure method

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { FaAngleDoubleRight } from "react-icons/fa";
+import { FaWindowMinimize } from "react-icons/fa";
 import "./css/App.css";
 
 const url = "https://course-api.com/react-tabs-project";
@@ -7,7 +8,7 @@ const url = "https://course-api.com/react-tabs-project";
 function App() {
   const [loading, setLoading] = useState(true);
   const [jobs, setJobs] = useState([]);
-  const [value, setVlue] = useState(0);
+  const [value, setValue] = useState(0);
 
   const fetchJobs = async () => {
     const response = await fetch(url);
@@ -35,9 +36,21 @@ function App() {
     <section className="section">
       <div className="home-container container">
         <div className="row">
-          <div className="col-md-4">wqeqeq</div>
+          <div className="col-md-4" style={{ marginTop: "120px" }}>
+            {jobs.map((item, index) => {
+              return (
+                <div class="btn-groups">
+                  <button key={item.id} onClick={() => setValue(index)}>
+                    {item.company}
+                  </button>
+                </div>
+              );
+            })}
+          </div>
+
           <div className="col-md-8">
             <h3 className="experience">Experience</h3>
+            <FaWindowMinimize className="experience-icon"></FaWindowMinimize>
             <div className="experience-wrapper">
               <p className="title">{title}</p>
               <button class="btn btn-light btn-name">
@@ -48,14 +61,13 @@ function App() {
                 return (
                   <div className="job-desc" key={index}>
                     <FaAngleDoubleRight className="job-icon"></FaAngleDoubleRight>
-                    <p style={{ marginLeft: "20px", marginTop: "-24px" }}>
+                    <p style={{ marginLeft: "40px", marginTop: "-35px" }}>
                       {duty}
                     </p>
                   </div>
                 );
               })}
             </div>
-
             <button type="button" class="btn btn-light more-info">
               More Info
             </button>
